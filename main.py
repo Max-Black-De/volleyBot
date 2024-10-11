1d6638ed386c27e95370e6af71e78f055e7a8241
Max-Black-De <black_de@yahoo.com>
2024-10-11 23:16:18 +0500

    Added def remove_past_events

--- main.py
+++ main.py
@@ -37,22 +37,22 @@ def create_event_on_date(target_date):
     return event_id, event_name
 
 # Функция для удаления старых событий и очистки списка спортсменов
-# async def remove_past_events(context: ContextTypes.DEFAULT_TYPE):
-#     current_date = datetime.now().date()
-#     events_to_remove = []
-#
-#     for event_id, event_name in event_ids.items():
-#         # Извлекаем дату события из имени события
-#         event_date_str = event_name.split()[3:5]  # Получаем дату как строку из имени события
-#         event_date = datetime.strptime(" ".join(event_date_str), '%d %B').date()
-#
-#         if event_date < current_date:  # Если дата события уже прошла
-#             events_to_remove.append(event_id)
-#
-#     for event_id in events_to_remove:
-#         logger.info(f"Удаление события {event_ids[event_id]} с ID: {event_id}")
-#         del event_participants[event_id]  # Удаляем участников события
-#         del event_ids[event_id]  # Удаляем само событие
+async def remove_past_events(context: ContextTypes.DEFAULT_TYPE):
+    current_date = datetime.now().date()
+    events_to_remove = []
+
+    for event_id, event_name in event_ids.items():
+        # Извлекаем дату события из имени события
+        event_date_str = event_name.split()[3:5]  # Получаем дату как строку из имени события
+        event_date = datetime.strptime(" ".join(event_date_str), '%d %B').date()
+
+        if event_date < current_date:  # Если дата события уже прошла
+            events_to_remove.append(event_id)
+
+    for event_id in events_to_remove:
+        logger.info(f"Удаление события {event_ids[event_id]} с ID: {event_id}")
+        del event_participants[event_id]  # Удаляем участников события
+        del event_ids[event_id]  # Удаляем само событие
 
 # Функция для определения ближайшего тренировочного дня
 def get_next_training_day():
